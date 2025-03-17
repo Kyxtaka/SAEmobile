11e156ccc114184d0040951eca5e1846fbe7bd5b
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-03-17 17:00:31 +0100

    modifications accueil et barre de recherche

--- lib/UI/accueil.dart
+++ lib/UI/accueil.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:saemobile/UI/global/header.dart';
+import 'package:saemobile/UI/themes/searchBar.dart';
+
 class Accueil extends StatelessWidget{
   static Header header = new Header();
+  static searchBar barreRecherche = new searchBar();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: header.create(),
         body: Center(
-
+          child:Column(
+            mainAxisSize: MainAxisSize.min,
+            children:[
+              barreRecherche,
+            ],
         )
+    )
     );
   }
 }
--- lib/UI/themes/searchBar.dart
+++ lib/UI/themes/searchBar.dart
@@ -6,44 +6,46 @@ class searchBar extends StatefulWidget {
   State<SearchBar> createState() => _SearchBarState();
 }
 class _SearchBarState extends State<SearchBar> {
-@override
-Widget build(BuildContext context) {
-  return Container(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Search Bar Sample')),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SearchAnchor(
-            builder: (BuildContext context, SearchController controller) {
-              return SearchBar(
-                controller: controller,
-                padding: const WidgetStatePropertyAll<EdgeInsets>(
-                  EdgeInsets.symmetric(horizontal: 16.0),
-                ),
-                onTap: () {
-                  controller.openView();
-                },
-                onChanged: (_) {
-                  controller.openView();
-                },
-                leading: const Icon(Icons.search),
-              );
-            },
-            suggestionsBuilder: (BuildContext context, SearchController controller) {
-              return List<ListTile>.generate(5, (int index) {
-                final String item = 'item $index';
-                return ListTile(
-                  title: Text(item),
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Scaffold(
+          appBar: AppBar(title: const Text('Search Bar Sample')),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SearchAnchor(
+              builder: (BuildContext context, SearchController controller) {
+                return SearchBar(
+                  controller: controller,
+                  padding: const WidgetStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
                   onTap: () {
-                    setState(() {
-                      controller.closeView(item);
-                    });
+                    controller.openView();
                   },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                  leading: const Icon(Icons.search),
                 );
-              });
-            },
+              },
+              suggestionsBuilder: (BuildContext context,
+                  SearchController controller) {
+                return List<ListTile>.generate(5, (int index) {
+                  final String item = 'item $index';
+                  return ListTile(
+                    title: Text(item),
+                    onTap: () {
+                      setState(() {
+                        controller.closeView(item);
+                      });
+                    },
+                  );
+                });
+              },
+            ),
           ),
-        ),
-      )
-  );
+        )
+    );
+  }
 }
