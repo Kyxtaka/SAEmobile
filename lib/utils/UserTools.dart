fe51309c9e96a17c95b1d7613c1b41750cecb59e
clemence <clemence.bcq@gmail.com>
2025-03-20 11:56:34 +0100

    connexion avec la base de données

--- lib/utils/UserTools.dart
+++ lib/utils/UserTools.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 ///  gestion de la connexion, de l'inscription et déconnexion
 class UserTools {
-  final SupabaseClient supabase = Supabase.instance.client;
+  late SupabaseClient supabase = Supabase.instance.client;
 
   /// Vérifie si un utilisateur est connecté
   bool isConnected() {
@@ -12,13 +14,41 @@ class UserTools {
   /// Connexion avec email et mot de passe
   Future<String?> login(String email, String password) async {
     try {
-      await supabase.auth.signInWithPassword(email:"clemence@mail.com","motdepasse");
-      print("connected");
-      return null;
+      final supabase = _initDb();
+      final result = await this.supabase.from("Visiteur").select('mail, password').eq('mail', email);
+      if (result.isNotEmpty) {
+        print(result[0]);
+        print(password);
+        if(result[0]['mail']==email && result[0]['password']==password){
+          print("connected");
+          return null;
+        }
+        else {
+          throw new Exception("Email ou mot de passe incorrect");
+        }
+      }
+      else {
+        throw new Exception("Erreur avec la base de données");
+      }
     } catch (error) {
       print(error);
       return error.toString();
     }
+  }
+  Future<SupabaseClient> _initDb() async {
+    try {
+      await dotenv.load(fileName: ".env");
+      await Supabase.initialize(
+          url: dotenv.env['SUPABASE_DB_API_URL']??'',
+          anonKey: dotenv.env['SUPABASE_ANON_KEY']??''
+      );
+      this.supabase = Supabase.instance.client;
+      return Supabase.instance.client;
+    } catch (e) {
+      debugPrint("supabase and are already initialized");
+      return Supabase.instance.client;
+    }
+
   }
 
   /// Déconnexion de l'utilisateur
@@ -26,3 +56,5 @@ class UserTools {
     await supabase.auth.signOut();
   }
 }
+
+
