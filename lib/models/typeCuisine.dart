a20e7209d63333633b5920d0ec7b3a98ebe377c7
Akhtar Naima <naimasanaakhtar@gmail.com>
2025-03-22 12:40:05 +0100

    ajout de méthode pour la table typeCuisine

--- lib/models/typeCuisine.dart
+++ lib/models/typeCuisine.dart
@@ -1,23 +1,23 @@
 class TypeCuisine {
-  final int _idType;
+  final int _idTypeCuisine;
   final String _cuisine;
 
   const TypeCuisine(
-      this._idType,
+      this._idTypeCuisine,
       this._cuisine);
 
-  int get idType => _idType;
+  int get id => _idTypeCuisine;
 
   String get cuisine => _cuisine;
 
   void debugPrint() {
-    String typeCuisine = "idType: $_idType, cuisine: $_cuisine";
+    String typeCuisine = "idTypeCuisine: $_idTypeCuisine, cuisine: $_cuisine";
     print(typeCuisine);
   }
 
   Map<String, Object?> toMapLocal() {
     return {
-      'idType': _idType,
+      'idTypeCuisine': _idTypeCuisine,
       'cuisine': _cuisine,
     };
   }
--- lib/services/local/tables/typeCuisineTable.dart
+++ lib/services/local/tables/typeCuisineTable.dart
@@ -1,2 +1,44 @@
+import 'package:sqflite/sqflite.dart';
+import '../../../models/typeCuisine.dart';
+import '../sqlfliteDatabase.dart';
+
 class TypeCuisineTable {
+  Future<void> insertTypeCuisine(TypeCuisine typeCuisine) async {
+    final db = await SqlfliteDatabase.instance.database;
+     await db.insert(
+      'TypeCuisine',
+      typeCuisine.toMapLocal(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<void> updateTypeCuisine(TypeCuisine typeCuisine) async {
+    final db = await SqlfliteDatabase.instance.database;
+    await db.update(
+      'TypeCuisine',
+      typeCuisine.toMapLocal(),
+      where: 'id = ?',
+      whereArgs: [typeCuisine.id],
+    );
+  }
+
+  Future<void> deleteTypeCuisine(int id) async {
+    final db = await SqlfliteDatabase.instance.database;
+    await db.delete(
+      'TypeCuisine',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<List<TypeCuisine>> getAllTypeCuisines() async {
+    final db = await SqlfliteDatabase.instance.database;
+    final List<Map<String, Object?>> typeCuisineMaps = await db.query('TypeCuisine');
+    return typeCuisineMaps.map((map) {
+      return TypeCuisine(
+        map['idTypeCuisine'] as int,
+        map['cuisine'] as String
+      );
+    }).toList();  }
 }
+
