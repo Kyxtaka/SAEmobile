bd11acd2a79544ff026b0b70311b0fa236ee62ad
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-03-25 17:00:38 +0100

    modification condition de connection login (UserTools)

--- lib/utils/UserTools.dart
+++ lib/utils/UserTools.dart
@@ -20,45 +20,35 @@ class UserTools {
   /// Connexion avec email et mot de passe
   Future<String?> login(String email, String password) async {
     try {
-      //final supabase = _initDb();
-      final result = await this.supabase.from("Visiteur").select('mail, password').eq('mail', email);
-      if (result.isNotEmpty) {
-        print(result[0]);
+      final result = await this.supabase
+          .from("Visiteur")
+          .select('mail, password')
+          .eq('mail', email)
+          .maybeSingle();
+
+      if (result != null && result['mail']==email && result['password']==password) {
+        print(result);
         print(password);
-        if(result[0]['mail']==email && result[0]['password']==password){
+        print("connected");
+        return null;
+        /*
+        if(result['mail']==email && result['password']==password){
           print("connected");
           return null;
         }
         else {
           throw new Exception("Email ou mot de passe incorrect");
         }
+         */
       }
       else {
-        throw new Exception("Erreur avec la base de données");
+        throw new Exception("Email ou mot de passe incorrect");
       }
     } catch (error) {
       print(error);
       return error.toString();
     }
   }
-  /*
-  Future<SupabaseClient> _initDb() async {
-    try {
-      await dotenv.load(fileName: ".env");
-      await Supabase.initialize(
-          url: dotenv.env['SUPABASE_DB_API_URL']??'',
-          anonKey: dotenv.env['SUPABASE_ANON_KEY']??''
-      );
-      this.supabase = Supabase.instance.client;
-      return Supabase.instance.client;
-    } catch (e) {
-      debugPrint("supabase and are already initialized");
-      return Supabase.instance.client;
-    }
-
-  }
-
-   */
 
   Future<String?> signin(String nom, String prenom, String email, String password, field) async {
     try {
