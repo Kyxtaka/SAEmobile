b5d8bc72afb6ba14359e5d31c76e48b9d64f33d3
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-03-28 23:17:31 +0100

    separation de dropdown en 2 class widget

--- lib/UI/research/saerchpage.dart
+++ lib/UI/research/saerchpage.dart
@@ -8,6 +8,7 @@ import 'package:saemobile/api/carateristiqueandcuisineapi.dart';
 import 'package:saemobile/models/caracteristique.dart';
 import 'package:saemobile/models/typeCuisine.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
 class SearchScreen extends StatefulWidget {
   final SupabaseClient database = Supabase.instance.client;
   @override
@@ -61,6 +62,7 @@ class _SearchScreenState extends State<SearchScreen> {
                   OutlinedButton(
                       onPressed: () => {},
                       child: Text("En attente de la barre de recherche")),
+
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
@@ -77,114 +79,25 @@ class _SearchScreenState extends State<SearchScreen> {
                   ),
 
                   // TypeCuisine Dropdown
-                  DropdownButtonHideUnderline(
-                    child: DropdownButton2<TypeCuisine>(
-                      isExpanded: true,
-                      hint: const Row(
-                        children: [
-                          Icon(
-                            Icons.list,
-                            size: 16,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(width: 4),
-                          Expanded(
-                            child: Text(
-                              'Sélectionnez un type de cuisine',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ],
-                      ),
-                      items: typeCuisines.map<DropdownMenuItem<TypeCuisine>>(
-                            (TypeCuisine item) {
-                          return DropdownMenuItem<TypeCuisine>(
-                            value: item,
-                            child: Text(
-                              item.cuisine,
-                              style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          );
-                        },
-                      ).toList(),
-                      value: selectedType,
-                      onChanged: (TypeCuisine? valueType) {
-                        setState(() {
-                          selectedType = valueType;
-                        });
-                      },
-                    ),
+                  DropdownTypeCuisine(
+                    typeCuisines: typeCuisines,
+                    selectedType: selectedType,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedType = value;
+                      });
+                    },
                   ),
 
-
                   // Caracteristique Dropdown
-                  DropdownButtonHideUnderline(
-                    child: DropdownButton2<Caracteristique>(
-                      isExpanded: true,
-                      hint: selectedCarac == null
-                          ? const Row(
-                        children: [
-                          Icon(
-                            Icons.list,
-                            size: 16,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(width: 4),
-                          Expanded(
-                            child: Text(
-                              'Sélectionnez une caractéristique',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ],
-                      )
-                          : Text(
-                        selectedCarac!.carac,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      items: caracteristiques.map<DropdownMenuItem<Caracteristique>>(
-                            (Caracteristique item) {
-                          return DropdownMenuItem<Caracteristique>(
-                            value: item,
-                            child: Text(
-                              item.carac,
-                              style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          );
-                        },
-                      ).toList(),
-                      value: selectedCarac,
-                      onChanged: (Caracteristique? valueCarac) {
-                        setState(() {
-                          selectedCarac = valueCarac;
-                        });
-                      },
-                    ),
+                  DropdownCaracteristique(
+                    caracteristiques: caracteristiques,
+                    selectedCarac: selectedCarac,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedCarac = value;
+                      });
+                    },
                   ),
 
                   ElevatedButton(
@@ -214,6 +127,147 @@ class _SearchScreenState extends State<SearchScreen> {
 
           return Container();
         },
+      ),
+    );
+  }
+}
+
+class DropdownTypeCuisine extends StatefulWidget {
+  final List<TypeCuisine> typeCuisines;
+  final ValueChanged<TypeCuisine?> onChanged;
+  final TypeCuisine? selectedType;
+
+  const DropdownTypeCuisine({
+    required this.typeCuisines,
+    required this.onChanged,
+    this.selectedType,
+    super.key,
+  });
+
+  @override
+  _DropdownTypeCuisineState createState() => _DropdownTypeCuisineState();
+}
+
+class _DropdownTypeCuisineState extends State<DropdownTypeCuisine> {
+  Widget build(BuildContext context) {
+    return DropdownButtonHideUnderline(
+      child: DropdownButton2<TypeCuisine>(
+        isExpanded: true,
+        hint: const Row(
+          children: [
+            Icon(
+              Icons.list,
+              size: 16,
+              color: Colors.deepOrangeAccent,
+            ),
+            SizedBox(width: 4),
+            Expanded(
+              child: Text(
+                'Sélectionnez un type de cuisine',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        ),
+        items: widget.typeCuisines.map<DropdownMenuItem<TypeCuisine>>(
+              (TypeCuisine item) {
+            return DropdownMenuItem<TypeCuisine>(
+              value: item,
+              child: Text(
+                item.cuisine,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            );
+          },
+        ).toList(),
+        value: widget.selectedType,
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
+}
+
+class DropdownCaracteristique extends StatefulWidget {
+  final List<Caracteristique> caracteristiques;
+  final ValueChanged<Caracteristique?> onChanged;
+  final Caracteristique? selectedCarac;
+
+  const DropdownCaracteristique({
+    required this.caracteristiques,
+    required this.onChanged,
+    this.selectedCarac,
+    super.key,
+  });
+
+  @override
+  _DropdownCaracteristiqueState createState() => _DropdownCaracteristiqueState();
+}
+
+class _DropdownCaracteristiqueState extends State<DropdownCaracteristique> {
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonHideUnderline(
+      child: DropdownButton2<Caracteristique>(
+        isExpanded: true,
+        hint: widget.selectedCarac == null
+            ? const Row(
+          children: [
+            Icon(
+              Icons.list,
+              size: 16,
+              color: Colors.deepOrangeAccent,
+            ),
+            SizedBox(width: 4),
+            Expanded(
+              child: Text(
+                'Sélectionnez une caractéristique',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        )
+            : Text(
+          widget.selectedCarac!.carac,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          overflow: TextOverflow.ellipsis,
+        ),
+        items: widget.caracteristiques.map<DropdownMenuItem<Caracteristique>>(
+              (Caracteristique item) {
+            return DropdownMenuItem<Caracteristique>(
+              value: item,
+              child: Text(
+                item.carac,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            );
+          },
+        ).toList(),
+        value: widget.selectedCarac,
+        onChanged: widget.onChanged,
       ),
     );
   }
