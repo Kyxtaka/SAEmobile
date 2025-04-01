395f60e8574bc468703c243dab55d7f166e88e4f
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-04-01 02:17:00 +0200

    Ajout chargement critique img dans la page de resto

--- lib/UI/review/critiquesRestaurant.dart
+++ lib/UI/review/critiquesRestaurant.dart
@@ -11,6 +11,12 @@ class CritiqueRestaurants extends StatelessWidget{
   final String restaurantId;
   CritiqueRestaurants({required this.restaurantId});
 
+  Future<void> _initWidgetImg(List<Critique> critiques) async{
+    for(Critique crit in critiques) {
+      await crit.getCritiqueImageIfExist();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var footer = Footer();
@@ -20,49 +26,40 @@ class CritiqueRestaurants extends StatelessWidget{
         appBar: Header.create(),
         body: FutureBuilder<List<Critique>>(
           future: CritiqueAPI.getCritiquesForRestaurant(restaurantId),
-          builder: (context, snapshot){
+          builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const CircularProgressIndicator();
-            }
-            else if (snapshot.hasError) {
+            } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
             } else if (snapshot.hasData) {
-              return  ListView.builder(
-              itemCount: snapshot.data?.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final critique = snapshot.data?[index];
-                  if (critique == null) {
-                    return Text("Erreur dans la récupération d'une review");
+              for (var i = 0 ; i < snapshot.data!.length; i++) {
+                snapshot.data![i].getCritiqueImageIfExist();
+              }
+              return  FutureBuilder(
+                  future: _initWidgetImg(snapshot.data!),
+                  builder: (context, snapshot2) {
+                    if (snapshot2.connectionState != ConnectionState.done) {
+                      return Center(child: const CircularProgressIndicator(),);
+                    }
+
+                    if (snapshot2.hasError) {
+                      return Center(
+                        child: Text(snapshot2.error.toString()),
+                      );
+                    }
+
+                    return ListView.builder(
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final critique = snapshot.data?[index];
+                          if (critique == null) {
+                            return Text("Erreur dans la récupération d'une review");
+                          }
+                          return critique.renderCard(context);
+                        }
+                    );
                   }
-                  return critique.renderCard(context);
-                  // return Card(
-                  //     child: ListTile(
-                  //         title: Text(
-                  //           '${critique.user?.prenom} ${critique.user?.nom} avez critiqué le ${critique
-                  //               .date_test}',
-                  //         ),
-                  //
-                  //         /// cette partie a été généré à l'aide d'une IA générative
-                  //         subtitle: Column(
-                  //             crossAxisAlignment: CrossAxisAlignment.start,
-                  //             children: [
-                  //               RatingBarIndicator(
-                  //                 rating: critique.note.toDouble(),
-                  //                 itemBuilder: (context, index) =>
-                  //                     Icon(
-                  //                       Icons.star,
-                  //                       color: Colors.amber,
-                  //                     ),
-                  //                 itemCount: 5,
-                  //                 itemSize: 24.0,
-                  //                 direction: Axis.horizontal,
-                  //               ),
-                  //               SizedBox(height: 8),
-                  //               Text(critique.message)
-                  //             ])
-                  //     )
-                  // );
-                });
+              );
             }
             return Text("Pas de review");
           }
