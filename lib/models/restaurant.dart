6dbb126a9364ef162736a732da1f7f1630e1d0aa
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-04-02 17:59:52 +0200

    ajout autorisation pour voir la liste de toutes les app installé sur l'appareil

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -145,7 +145,7 @@ class Restaurant {
                     ),
                 ),
               ),
-              // hyperLink(context),
+
             ],
           ),
           title: Text(name ?? ""),
